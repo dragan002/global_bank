1cf604c458ba1b2dd70ddc3af531dc50a036251b
dragan002 <draganvujic29@gmail.com>
2024-01-06 12:34:06 +0100

    Find a single record

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,9 +1,33 @@
-<?php
- require_once('../../../private/initialize.php');
+<?php require_once('../../../private/initialize.php');
 
-echo htmlspecialchars($id = $_GET['id'] ?? '1');;
-?>
 
-<a href="show.php?name=<?php echo u("John Doe"); ?>">Link1</a>
-<a href="show.php?company=<?php echo u("Widgets&More"); ?>">Link2</a>
-<a href="show.php?query=<?php echo u("!#*?"); ?>">Link3</a>
+$id = $_GET['id'] ?? '1'; // PHP > 7.0
+$subject = find_by_id($id);
+
+$page_title = 'Show Page'; 
+include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="subject show">
+    <h1>Subject <?php echo h($subject['menu_name']);?></h1>
+    <div class="attributes">
+        <dl>
+            <dt>Menu name</dt>
+            <dd><?php echo h($subject['menu_name']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Position</dt>
+            <dd><?php echo h($subject['position']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Visible</dt>
+            <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+        </dl>
+    </div>
+  </div>
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
